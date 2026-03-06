2fd31291d50122cbdba7dae4c2766aaae1f8b63f
Hiya Verma <hiyaverm@bu.edu>
2026-03-06 10:30:32 -0500

    finished Nav.tsx

--- mp-3/src/components/Nav.tsx
+++ mp-3/src/components/Nav.tsx
@@ -1,16 +1,70 @@
-import {Link} from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default Nav(){
+const StyledNav = styled.nav`
+    width: 30%; 
+    background-color: #DDD5E7; 
+    padding: 20px;
+    min-height: 100%;
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    li {
+        width: 100%;
+    }
+
+    a {
+        text-decoration: none;
+        color: #1B065E;
+        background-color: #FBFBF2;
+        display: block;
+        padding: 15px;
+        border-radius: 8px;
+        text-align: center;
+        font-weight: bold;
+        font-size: calc(14px + 0.5vw);
+        transition: all 0.3s ease;
+
+        &:hover {
+            background-color: #759FBC;
+            color: white;
+            transform: scale(1.02);
+        }
+    }
+
+    @media screen and (max-width: 1000px) {
+        width: 100%;
+        min-height: auto;
+        
+        ul {
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+
+        li {
+            width: auto;
+        }
+    }
+`;
+
+export default function Nav(){
     return(
-        <nav>
+        <StyledNav>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="edu.html">Education</Link></li>
-                <li><Link to="emp.html">Employment</Link></li>
-                <li><Link to="awards.html">Awards</Link></li>
-                <li><Link to="projects.html">Projects</Link></li>
-                <li><Link to="doc.html">Documents</Link></li>
+                <li><Link to="/education">Education</Link></li>
+                <li><Link to="/experiences">Experiences</Link></li>
+                <li><Link to="/certifications">Certifications</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/documents">Documents</Link></li>
             </ul>
-        </nav>
+        </StyledNav>
     );
 }
--- mp-3/src/components/mains/Documents.tsx
+++ mp-3/src/components/mains/Documents.tsx
@@ -1,6 +1,57 @@
+import styled from 'styled-components';
+
+const StyledDocs = styled.main`
+    padding: 40px;
+    flex: 1;
+    width: 70%;
+
+    h3 {
+        margin-bottom: 20px;
+        color: #1B065E;
+        border-bottom: 2px solid #DDD5E7;
+        display: inline-block;
+        font-size: calc(18px + 0.8vw);
+
+        &:not(:first-child) {
+            margin-top: 40px;
+        }
+    }
+
+    p {
+        font-size: calc(14px + 0.2vw);
+        margin-bottom: 15px;
+        line-height: 1.5;
+    }
+
+    ul {
+        list-style-type: none; /* Removing default bullets for a cleaner look */
+        padding-left: 0;
+        
+        li {
+            background-color: #FBFBF2;
+            margin-bottom: 10px;
+            padding: 10px 15px;
+            border-left: 5px solid #759FBC;
+            border-radius: 4px;
+            font-size: calc(14px + 0.2vw);
+            transition: transform 0.2s ease;
+            cursor: pointer;
+
+            &:hover {
+                transform: translateX(5px);
+                background-color: #DDD5E7;
+            }
+        }
+    }
+
+    @media screen and (max-width: 1000px) {
+        width: 100%;
+    }
+`;
+
 export default function Documents() {
     return(
-        <main>
+        <StyledDocs>
             <h3>Downloadable Documents</h3>
                     <p>Please find my resume and transcripts below:</p>
                     
@@ -14,6 +65,6 @@ export default function Documents() {
                     <br/>
                     <h3>References</h3>
                     <p>References are available upon request.</p>
-        </main>
+        </StyledDocs>
     )
 }
