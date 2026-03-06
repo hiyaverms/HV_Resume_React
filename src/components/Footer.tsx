b485527cad87b75e6e3be2cb87b7c040d8c7535e
Hiya Verma <hiyaverm@bu.edu>
2026-03-06 10:55:25 -0500

    finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,35 @@
-import {Link} from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default function (Footer){
-    return(
-        <p>All Rights Reserved by Hiya Verma <Link to=""> Credits</Link> &#169;</p>
-        <div id="time-date"></div>
+const StyledFooter = styled.footer`
+    background-color: #1B065E;
+    color: #FBFBF2;
+    padding: 20px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    a {
+        color: #759FBC;
+        text-decoration: none;
+        &:hover { text-decoration: underline; }
+    }
+
+    @media screen and (max-width: 1000px) {
+        flex-direction: column;
+        gap: 10px;
+    }
+`;
+
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <StyledFooter>
+            <p>
+                All Rights Reserved by Hiya Verma 
+                <Link to="/credits"> Credits</Link> © {currentYear}
+            </p>
+        </StyledFooter>
     );
 }
