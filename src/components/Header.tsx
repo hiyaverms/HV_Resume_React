21f77586f0f7ca9cc1e4bcf7a484457c3cefffd6
Hiya Verma <hiyaverm@bu.edu>
2026-03-06 10:50:11 -0500

    finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,22 @@
-export default function Header(){
-    <header>
-        <h1>Hiya Verma</h1>
-        <p>My Resume</p>
-    </header>
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    display: flex;
+    flex-direction: column;
+    padding: 1vh 1vw;
+    background-color: #1B065E; 
+    color: #FBFBF2; 
+
+    @media screen and (max-width: 1000px) {
+        align-items: center;
+    }
+`;
+
+export default function Header() {
+    return (
+        <StyledHeader>
+            <h1>Hiya Verma</h1>
+            <p>My Resume</p>
+        </StyledHeader>
+    );
 }
